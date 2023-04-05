9d5dbb889df657051e68d4963d51b4ecb1df3f2c
Arctic Inferno <arcticinferno@protonmail.com>
2023-04-05 14:54:45 -0400

    Add some pkgs

--- config.el
+++ config.el
@@ -1,16 +1,15 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(package-refresh-contents)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+(package-refresh-contents)
 
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
-(setq use-package-always-ensure t)
-
-(use-package projectile
-  :config
-  (projectile-global-mode 1))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
 
 (use-package all-the-icons)
 
@@ -49,4 +48,40 @@
   (evil-collection-init))
 (use-package evil-tutor)
 
+(use-package projectile
+  :config
+  (projectile-global-mode 1))
+
+(use-package doom-modeline)
+(doom-modeline-mode 1)
+
+(use-package general
+  :config
+  (general-evil-setup t))
+
+(use-package doom-themes)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+(load-theme 'doom-one t)
+
+(use-package org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+
+
+
+
+
+
+(use-package haskell-mode)
+(use-package lua-mode)
+(use-package markdown-mode)
+
 (delete-selection-mode t)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(global-display-line-numbers-mode 1)
+(global-visual-line-mode t)
