35ae7aae54b91062e6cc2f66eebd65d50d250030
Arctic Inferno <arcticinferno@protonmail.com>
2023-04-05 15:06:06 -0400

    Add some shit

--- config.el
+++ config.el
@@ -85,3 +85,75 @@
 
 (global-display-line-numbers-mode 1)
 (global-visual-line-mode t)
+
+;; zoom in/out like we do everywhere else.
+  (global-set-key (kbd "C-=") 'text-scale-increase)
+  (global-set-key (kbd "C--") 'text-scale-decrease)
+  (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
+  (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+
+  (nvmap :keymaps 'override :prefix "SPC"
+	 "SPC"   '(counsel-M-x :which-key "M-x")
+	 "c c"   '(compile :which-key "Compile")
+	 "c C"   '(recompile :which-key "Recompile")
+	 "h r r" '((lambda () (interactive) (load-file "~/.emacs.d/init.el")) :which-key "Reload emacs config")
+	 "t t"   '(toggle-truncate-lines :which-key "Toggle truncate lines"))
+  (nvmap :keymaps 'override :prefix "SPC"
+	 "m *"   '(org-ctrl-c-star :which-key "Org-ctrl-c-star")
+	 "m +"   '(org-ctrl-c-minus :which-key "Org-ctrl-c-minus")
+	 "m ."   '(counsel-org-goto :which-key "Counsel org goto")
+	 "m e"   '(org-export-dispatch :which-key "Org export dispatch")
+	 "m f"   '(org-footnote-new :which-key "Org footnote new")
+	 "m h"   '(org-toggle-heading :which-key "Org toggle heading")
+	 "m i"   '(org-toggle-item :which-key "Org toggle item")
+	 "m n"   '(org-store-link :which-key "Org store link")
+	 "m o"   '(org-set-property :which-key "Org set property")
+	 "m t"   '(org-todo :which-key "Org todo")
+	 "m x"   '(org-toggle-checkbox :which-key "Org toggle checkbox")
+	 "m B"   '(org-babel-tangle :which-key "Org babel tangle")
+	 "m I"   '(org-toggle-inline-images :which-key "Org toggle inline imager")
+	 "m T"   '(org-todo-list :which-key "Org todo list")
+	 "o a"   '(org-agenda :which-key "Org agenda")
+	 )
+  (set-face-attribute 'default nil
+    :font "Inconsolata"
+    :height 110
+    :weight 'medium)
+  (set-face-attribute 'variable-pitch nil
+    :font "Iosevka"
+    :height 120
+    :weight 'medium)
+  (set-face-attribute 'fixed-pitch nil
+    :font "Inconsolata"
+    :height 110
+    :weight 'medium)
+  ;; Makes commented text and keywords italics.
+  ;; This is working in emacsclient but not emacs.
+  ;; Your font must have an italic face available.
+  (set-face-attribute 'font-lock-comment-face nil
+    :slant 'italic)
+  (set-face-attribute 'font-lock-keyword-face nil
+    :slant 'italic)
+
+  ;; Uncomment the following line if line spacing needs adjusting.
+  (setq-default line-spacing 0.12)
+
+  ;; Needed if using emacsclient. Otherwise, your fonts will be smaller than expected.
+  (add-to-list 'default-frame-alist '(font . "Inconsolata-17"))
+  ;; changes certain keywords to symbols, such as lamda!
+  (setq global-prettify-symbols-mode t)
+  (nvmap :states '(normal visual) :keymaps 'override :prefix "SPC"
+	 "."     '(find-file :which-key "Find file")
+	 "f f"   '(find-file :which-key "Find file")
+	 "f r"   '(counsel-recentf :which-key "Recent files")
+	 "f s"   '(save-buffer :which-key "Save file")
+	 "f u"   '(sudo-edit-find-file :which-key "Sudo find file")
+	 "f y"   '(dt/show-and-copy-buffer-path :which-key "Yank file path")
+	 "f C"   '(copy-file :which-key "Copy file")
+	 "f D"   '(delete-file :which-key "Delete file")
+	 "f R"   '(rename-file :which-key "Rename file")
+	 "f S"   '(write-file :which-key "Save file as...")
+	 "f U"   '(sudo-edit :which-key "Sudo edit file"))
+  (global-set-key (kbd "C-c") 'keyboard-quit)
+(define-key evil-insert-state-map (kbd "C-c") 'evil-normal-state)
+(define-key evil-normal-state-map (kbd "C-c") 'evil-normal-state)
